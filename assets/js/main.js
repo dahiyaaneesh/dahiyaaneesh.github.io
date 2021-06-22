0cbf4a9c2d0ce3c88dac429731a90cbd3232818a
aneesh_dahiya <aneesh@barix.com>
2021-06-22 17:16:04 +0200

    publication added, navigation bar hidden

--- assets/js/main.js
+++ assets/js/main.js
@@ -115,3 +115,14 @@
 			});
 
 })(jQuery);
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
